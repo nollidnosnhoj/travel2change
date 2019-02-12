f8a9824987aa6f5b4649f9514df030097bbdbe1a
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-02-11 23:16:05 -1000

    implemented static pages

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from crispy_forms.bootstrap import PrependedText
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import (
     authenticate, get_user_model
@@ -19,28 +22,65 @@ class CustomUserChangeForm(UserChangeForm):
         fields = ('email', 'first_name', 'last_name',)
 
 class LoginForm(forms.Form):
-    email       = forms.EmailField(label="Email")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'authform'
+        self.helper.form_show_labels = False
+        self.helper.layout = Layout(
+            PrependedText(
+                'email', 
+                '<i class="fa fa-envelope"></i>', 
+                placeholder='Email Address'
+            ),
+            PrependedText(
+                'password',
+                '<i class="fa fa-key"></i>', 
+                placeholder='Password',
+            ),
+            Submit('submit', 'Login', css_class='btn-success btn-lg btn-block')
+        )
+    
+    email       = forms.EmailField()
     password    = forms.CharField(widget=forms.PasswordInput)
 
 class RegisterForm(forms.ModelForm):
-    password1 = forms.CharField(
-        label='Password',
-        widget=forms.PasswordInput,
-    )
-    password2 = forms.CharField(
-        label='Confirm Password',
-        widget=forms.PasswordInput,
-    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'authform'
+        self.helper.form_show_labels = False
+        self.helper.layout = Layout(
+            PrependedText(
+                'email', 
+                '<i class="fa fa-envelope"></i>', 
+                placeholder='Email Address'
+            ),
+            PrependedText(
+                'first_name',
+                '<i class="fa fa-user"></i>', 
+                placeholder='First Name'
+            ),
+            PrependedText(
+                'last_name',
+                '<i class="fa fa-user"></i>', 
+                placeholder='Last Name'
+            ),
+            PrependedText(
+                'password',
+                '<i class="fa fa-key"></i>', 
+                placeholder='Password',
+            ),
+            Submit('submit', 'Register', css_class='btn-success btn-lg btn-block')
+        )
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name')
-    
-    def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Password don't match")
-        return password2
+        fields = ('email', 'first_name', 'last_name', 'password')
+
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
 
     def save(self, commit=True):
         user = super().save(commit=False)
