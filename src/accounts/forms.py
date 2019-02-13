e1883510e1bf22998f80a48c6483a990fc0f600f
Randall D. Johnson <randalldillonjohnson@hotmail.com>
2019-02-12 19:23:26 -1000

    fixed minor error

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -84,7 +84,7 @@ class RegisterForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password1'])
+        user.set_password(self.cleaned_data['password'])
         if commit:
             user.save()
         return user
