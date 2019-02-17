3a0bc18d9fa2d0eaeef58140bb2157982f4f41ae
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-02-16 16:53:06 -1000

    Updated user manager

--- src/accounts/managers.py
+++ src/accounts/managers.py
@@ -2,8 +2,7 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import ugettext_lazy as _
 
 class CustomUserManager(BaseUserManager):
-    # Creates Active User
-    def create_user(self, email, first_name, last_name, password, **extra_fields):
+    def _create_user(self, email, first_name, last_name, password, **extra_fields):
         if not email:
             raise ValueError(_('Email Address is required'))
         email = self.normalize_email(email)
@@ -14,10 +13,15 @@ class CustomUserManager(BaseUserManager):
             **extra_fields
         )
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
+        # Create Profile Object
         return user
 
-    # Creates Super User
+    def create_user(self, email, first_name, last_name, password, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, first_name, last_name, password, **extra_fields)
+
     def create_superuser(self, email, first_name, last_name, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
@@ -27,13 +31,12 @@ class CustomUserManager(BaseUserManager):
             raise ValueError(_('Superuser must have is_staff = True'))
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_superuser = True'))
-        return self.create_user(email, first_name, last_name, password, **extra_fields)
+        return self._create_user(email, first_name, last_name, password, **extra_fields)
 
-    # Create Staff User
     def create_staffuser(self, email, first_name, last_name, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_active', True)
         
         if extra_fields.get('is_staff') is not True:
             raise ValueError(_('Staffuser must have is_staff = True'))
-        return self.create_user(email, first_name, last_name, password, **extra_fields)
+        return self._create_user(email, first_name, last_name, password, **extra_fields)
