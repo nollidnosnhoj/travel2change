83975832bcc73bd59d25d5ae93cab292e8238e83
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-03-03 17:45:42 -1000

    Started implenting form wizard for activity creation

--- src/activity/urls.py
+++ src/activity/urls.py
@@ -1,13 +1,24 @@
 from django.conf.urls import url
+from .forms import (
+    RegionActivityForm, 
+    TitleActivityForm, 
+    AboutActivityForm,
+    AddressActivityForm,
+    LocationActivityForm,
+    ImagesActivityForm,
+)
+from .views import ActivityDetailView, ActivityWizard
 
-from . import views
-from .views import ActivityDetailView
+ACTIVITY_CREATE_STEP_FORMS = [
+    ("01", RegionActivityForm),
+    ("02", TitleActivityForm),
+    ("03", AboutActivityForm),
+    ("04", AddressActivityForm),
+    ("05", LocationActivityForm),
+    ("06", ImagesActivityForm),
+]
 
 urlpatterns = [
-    url(
-        r'^(?P<slug>[-\w]+)/$', 
-        ActivityDetailView.as_view(),
-        name='activity_detail'
-    ),
-
+    url(r'^view/(?P<slug>[\w-]+)/$', ActivityDetailView.as_view(), name='activity_detail'),
+    url(r'^create/$', ActivityWizard.as_view(ACTIVITY_CREATE_STEP_FORMS), name="activity_wizard"),
 ]
--- src/activity/views.py
+++ src/activity/views.py
@@ -1,9 +1,35 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-
+from formtools.wizard.views import SessionWizardView
 from .models import Activity
 
 class ActivityDetailView(DetailView):
     template_name = 'activity/activity_detail.html'
     model = Activity
     context_object_name = 'activity'
+
+TEMPLATES = {
+    "01": "activity/create/default.html",
+    "02": "activity/create/default.html",
+    "03": "activity/create/default.html",
+    "04": "activity/create/default.html",
+    "05": "activity/create/default.html",
+    "06": "activity/create/default.html",
+}
+
+class ActivityWizard(SessionWizardView, LoginRequiredMixin):
+    
+    def get_template_names(self):
+        return [TEMPLATES[self.steps.current]]
+
+    def done(self, form_list, **kwargs):
+        return render(self.request, 'done.html', {
+            'form_data': [form.cleaned_data for form in form_list]
+        })
+
+    def get(self, request, *args, **kwargs):
+        try:
+            return self.render(self.get_form())
+        except KeyError:
+            return super().get(request, *args, **kwargs)
