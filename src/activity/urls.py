731d11dffc2be1c2d886a2662078a13c19ff20bf
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-03-07 13:25:43 -1000

    removing 'activity' app

--- src/activity/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from .forms import ACTIVITY_CREATE_FORMS_LIST
-from .views import ActivityDetailView, ActivityWizard
-
-urlpatterns = [
-    url(r'^view/(?P<slug>[\w-]+)/$', ActivityDetailView.as_view(), name='activity_detail'),
-    url(r'^create/$', ActivityWizard.as_view(ACTIVITY_CREATE_FORMS_LIST), name="activity_wizard"),
-]
