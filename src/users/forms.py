95bc6f584382dcf41183e916cd91d86931019b2a
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-03-09 22:02:22 -1000

    added is_host checkbox in signup form

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import (
-    UserCreationForm, UserChangeForm,
-)
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.db import transaction
+from django.utils.translation import ugettext as _
 from allauth.account.forms import SignupForm as BaseSignupForm
+from hosts.models import Host
 
 User = get_user_model()
 
@@ -27,13 +28,22 @@ class SignupForm(BaseSignupForm):
         max_length=30,
         widget=forms.TextInput(attrs={'placeholder': 'First Name'})
     )
-    last_name = forms.CharField(
+    last_name  = forms.CharField(
         max_length=30,
         widget=forms.TextInput(attrs={'placeholder': 'Last Name'})
     )
-
+    is_host = forms.BooleanField(
+        required=False,
+        label=_('Will you be hosting activities?')
+    )
+    
+    @transaction.atomic
     def save(self, request):
         user = super().save(request)
+        is_host = self.cleaned_data['is_host']
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
+        if is_host:
+            host = Host.objects.create(user=user)
+            host.save()
         return user
