6e0e279903876972817db88f282e4b7960094fbd
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-03-10 22:12:22 -1000

    working on adding host's activities in profile

--- src/hosts/views.py
+++ src/hosts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
+from django.views.generic.list import ListView
+from .models import Host
+from activities.models import Activity
 
-# Create your views here.
+
+class HostDetailView(DetailView):
+    model = Host
+    template_name = 'hosts/host_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['activities'] = Activity.objects.filter(host=self.object)
+        return context
+
+
+class HostUpdateView(UpdateView):
+    pass
+
+
+class HostActivitiesListView(ListView):
+    model = Activity
+    template_name = 'hosts/host_activities_list.html'
+
+    def get_queryset(self):
+        return super().get_queryset().filter(host=)
