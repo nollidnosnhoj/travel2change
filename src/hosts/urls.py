efbb931f7c085f50d9b8dde6e0ddf06086ed5b86
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-03-12 10:30:21 -1000

    added hosts url

--- src/hosts/urls.py
+++ src/hosts/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url
+from django.urls import path
+from .views import HostDetailView, HostUpdateView
 
+app_name = 'hosts'
 urlpatterns = [
-    
+    path('<int:pk>/', HostDetailView.as_view(), name='detail'),
+    path('<int:pk>/update/', HostUpdateView.as_view(), name='update')
 ]
