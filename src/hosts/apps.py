42cebaf941871ae29a538481126751cecf2a6947
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-03-16 00:41:07 -1000

    Removed 'hosts' app

--- src/hosts/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from django.db.models.signals import post_save, pre_delete
-
-
-class HostsConfig(AppConfig):
-    name = 'hosts'
-
-    def ready(self):
-        import hosts.signals # noqa
