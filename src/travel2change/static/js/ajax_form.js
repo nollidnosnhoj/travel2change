a9674c4156d8ce7203f10a07ca744627ad4a9890
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-03-16 09:56:34 -1000

    ajax host update

--- src/travel2change/static/js/ajax_form.js
+++ src/travel2change/static/js/ajax_form.js
@@ -27,31 +27,3 @@ $.ajaxSetup({
         }
     }
 });
-
-$(document).ready(function(){
-    var $myForm = $('.ajax-form');
-    $myForm.submit(function(event){
-        event.preventDefault();
-        var $formData = $(this).serialize();
-        var $thisURL = $myForm.attr('data-url') || window.location.href;
-        $.ajax({
-            method: "POST",
-            url: $thisURL,
-            data: $formData,
-            success: handleFormSuccess,
-            error: handleFormError,
-        });
-    });
-
-    function handleFormSuccess(data, textStatus, jqXHR){
-        console.log(data);
-        console.log(textStatus);
-        console.log(jqXHR);
-    }
-
-    function handleFormError(jqXHR, textStatus, errorThrown){
-        console.log(jqXHR);
-        console.log(textStatus);
-        console.log(errorThrown);
-    }
-})
