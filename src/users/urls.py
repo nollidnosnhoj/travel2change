ad905bc6b9e762953609ba1513e3c7f6c634eb64
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-03-22 22:58:54 -1000

    last step before push to develop

--- src/users/urls.py
+++ src/users/urls.py
@@ -3,6 +3,6 @@ from .views import HostDetailView, HostUpdateView
 
 
 urlpatterns = [
-    path('hosts/<slug:slug>/', HostDetailView.as_view(), name="host_detail"),
     path('hosts/<slug:slug>/edit/', HostUpdateView.as_view(), name="host_update"),
+    path('hosts/<slug:slug>/', HostDetailView.as_view(), name="host_detail"),
 ]
