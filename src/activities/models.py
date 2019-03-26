0f56284d40215be37e90fac3d45943aab80ceb29
Iokepa <kep.lapera@gmail.com>
2019-03-25 17:07:53 -1000

    something

--- src/activities/models.py
+++ src/activities/models.py
@@ -165,6 +165,20 @@ class ActivityPhoto(models.Model):
     activity = models.ForeignKey(Activity, related_name='photos', on_delete=models.CASCADE)
     file = models.ImageField(upload_to=get_image_filename, verbose_name=_('Photo'))
 
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
 
 """                         ACTIVITY CMS PLUGINS                            """
 
