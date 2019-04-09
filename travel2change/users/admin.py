5635821756846d206ebf09785c4f246c9e9e60bd
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-04-08 19:46:53 -1000

    removed forms in admins

--- travel2change/users/admin.py
+++ travel2change/users/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from users.forms import CustomUserCreationForm, CustomUserChangeForm
 from users.models import Host
 
 User = get_user_model()
