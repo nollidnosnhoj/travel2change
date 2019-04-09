a4477498c5de4e962f5204207928b76076e0b37e
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-04-08 21:19:47 -1000

    removed activity review model from activities

--- travel2change/activities/models.py
+++ travel2change/activities/models.py
@@ -20,9 +20,6 @@ def get_photo_image_filename(instance, filename):
     """ Path where activity's photos are stored """
     return 'uploads/{0}/photos/{1}'.format(instance.activity.pk, filename)
 
-def get_review_image_filename(instance, filename):
-    return 'uploads/reviews/{0}/{1}/{2}'.format(instance.user.pk, instance.activity.pk, filename)
-
 
 class ActivityQuerySet(models.QuerySet):
     def approved(self):
@@ -207,29 +204,6 @@ class Activity(models.Model):
 class ActivityPhoto(models.Model):
     activity        = models.ForeignKey(Activity, related_name='photos', on_delete=models.CASCADE)
     file            = models.ImageField(upload_to=get_photo_image_filename, verbose_name=_('Photo'))
-
-
-class ActivityReview(models.Model):
-    RATING_CHOICES  = (
-        (1, 'Terrible'), (2, 'Poor'), (3, 'Average'), (4, 'Very Good'), (5, 'Exceptional'),
-    )
-    user            = models.ForeignKey(User, on_delete=models.CASCADE)
-    activity        = models.ForeignKey(Activity, on_delete=models.CASCADE, related_name=_('reviews'))
-    content         = models.TextField(_('content'), blank=False)
-    rating          = models.IntegerField(choices=RATING_CHOICES)
-    photo           = models.ImageField(_('review photo'), upload_to=get_review_image_filename)
-
-    show_name       = models.BooleanField(default=False)
-    show_email      = models.BooleanField(default=False)
-
-    created         = models.DateTimeField(auto_now_add=True)
-    modified        = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ('created', )
-    
-    def __str__(self):
-        return 'Review by {0} on {1}'.format(self.user, self.activity)
 
 
 """                         ACTIVITY CMS PLUGINS                            """
