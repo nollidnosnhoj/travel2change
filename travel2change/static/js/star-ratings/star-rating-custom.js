61286847c2c54559f10fb1e89629cc64c85651c7
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-04-11 10:55:07 -1000

    updated star rating

--- travel2change/static/js/star-ratings/star-rating-custom.js
+++ travel2change/static/js/star-ratings/star-rating-custom.js
@@ -1,12 +1,5 @@
 $('#input-rating').rating({
-    theme: 'krajee-fas',
-    starCaptions: {
-        1: 'Terrible',
-        2: 'Poor',
-        3: 'Average',
-        4: 'Very Good',
-        5: 'Exceptional',
-    },
-    'animate': false,
-    'size': 'sm',
+    size: 'sm',
+    displayOnly: false,
+    theme: 'krajee-fas'
 });
