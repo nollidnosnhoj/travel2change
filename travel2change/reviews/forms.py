5ead3cb596d75c8cbf2d4d88d0e7dc736b0a26cb
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-04-10 23:52:22 -1000

    changed rating plugin

--- travel2change/reviews/forms.py
+++ travel2change/reviews/forms.py
@@ -6,3 +6,10 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = ActivityReview
         fields = ('rating', 'content', 'photo', 'show_name', 'show_email', )
+        widgets = {'rating': forms.NumberInput(attrs={
+            'id': 'input-rating',
+            'stars': len(ActivityReview.RATING_CHOICES),
+            'min': 0,
+            'max': len(ActivityReview.RATING_CHOICES),
+            'step': 1,
+        })}
---
+++ travel2change/static/js/star-ratings/star-rating-custom.js
@@ -0,0 +1,12 @@
+$('#input-rating').rating({
+    theme: 'krajee-fas',
+    starCaptions: {
+        1: 'Terrible',
+        2: 'Poor',
+        3: 'Average',
+        4: 'Very Good',
+        5: 'Exceptional',
+    },
+    'animate': false,
+    'size': 'sm',
+});
