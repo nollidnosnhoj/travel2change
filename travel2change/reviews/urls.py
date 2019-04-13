ccf9c92b48ad77b1fafd6b96803edda88e75649d
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-04-13 00:10:00 -1000

    added delete review url

--- travel2change/reviews/urls.py
+++ travel2change/reviews/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import UpdateReview
+from .views import (
+    UpdateReview,
+    DeleteReview,
+)
 
 urlpatterns = [
     path('<int:pk>/update/', UpdateReview.as_view(), name="review_update"),
+    path('<int:pk>/delete/', DeleteReview.as_view(), name="review_delete"),
 ]
