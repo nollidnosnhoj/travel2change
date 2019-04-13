0bce924f3b7b220a6a2bf03a49a74bbdbaf4e0b6
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-04-12 15:05:00 -1000

    added review update and delete

--- travel2change/reviews/urls.py
+++ travel2change/reviews/urls.py
@@ -1,3 +1,6 @@
-urlpatterns = [
+from django.urls import path
+from .views import UpdateReview
 
+urlpatterns = [
+    path('<int:pk>/update/', UpdateReview.as_view(), name="review_update"),
 ]
--- travel2change/reviews/views.py
+++ travel2change/reviews/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import reverse, get_object_or_404
+from django.views.generic import DeleteView, UpdateView
+from .forms import ReviewForm
+from .models import Review
 
-# Create your views here.
+class UpdateReview(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Review
+    form_class = ReviewForm
+    context_object_name = "review"
+    template_name_suffix = "_update"
+    success_message = "Review successfully updated."
+
+    def dispatch(self, request, *args, **kwargs):
+        self.object = get_object_or_404(Review, pk=kwargs.get('pk'), user=request.user)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse('user_reviews')
+
+class DeleteReview(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    model = Review
+    success_message = "Review successfully removed."
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+    
+    def get_success_url(self):
+        return reverse('user_reviews')
