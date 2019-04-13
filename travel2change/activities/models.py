7b41b6abdea5d9d85016ebe634797e65692ad621
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-04-12 22:03:17 -1000

    added slugfield to region, tags, and categories

--- travel2change/activities/models.py
+++ travel2change/activities/models.py
@@ -27,7 +27,7 @@ class ActivityQuerySet(models.QuerySet):
 
 class Region(models.Model):
     name = models.CharField(max_length=60, blank=False)
-    slug = AutoSlugField(populate_from='name')
+    slug = models.SlugField(max_length=20, unique=True)
 
     objects = models.Manager()
 
@@ -40,6 +40,7 @@ class Region(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=60, blank=False, null=False, unique=True)
+    slug = models.SlugField(max_length=20, unique=True)
     font_awesome = models.CharField(max_length=60, blank=True, verbose_name=_('tag icon'),
         help_text=_('This will display an icon next to a tag. Format: fa-(icon name)'))
 
@@ -54,6 +55,7 @@ class Tag(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=60, blank=False, null=False, unique=True, verbose_name=_('activity category'))
+    slug = models.SlugField(max_length=20, unique=True)
     font_awesome = models.CharField(max_length=60, blank=True, verbose_name=_('category icon'),
         help_text=_('This will display an icon next to a tag. Format: fa-(icon name)'))
 
