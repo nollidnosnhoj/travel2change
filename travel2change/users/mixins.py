f863e39ef44681fbbb6814a4d7336915d7ef5ec2
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-04-13 00:08:45 -1000

    created listview interface for hosts

--- travel2change/users/mixins.py
+++ travel2change/users/mixins.py
@@ -1,4 +1,6 @@
 from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
 from users.models import Host
 
 class UserIsHostMixin(object):
@@ -7,3 +9,13 @@ class UserIsHostMixin(object):
         if not host:
             raise PermissionDenied
         return super().dispatch(request, *args, **kwargs)
+
+class HostListView(ListView):
+    def dispatch(self, request, *args, **kwargs):
+        self.host = get_object_or_404(Host, slug=kwargs['slug'])
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['host'] = self.host
+        return context
