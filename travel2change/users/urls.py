02ba434d1feadee9b178aac097ffb9546be379f2
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-04-13 00:08:32 -1000

    Added URL to HostReviewList view

--- travel2change/users/urls.py
+++ travel2change/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from users.views import (
+    HostReviewsListView,
     HostActivitiesListView,
     HostDetailView,
     HostUpdateView,
@@ -9,6 +10,7 @@ from users.views import (
 
 
 urlpatterns = [
+    path('hosts/<slug:slug>/reviews/', HostReviewsListView.as_view(), name='host_reviews'),
     path('hosts/<slug:slug>/activities/', HostActivitiesListView.as_view(), name="host_activities"),
     path('hosts/<slug:slug>/', HostDetailView.as_view(), name="host_detail"),
     path('accounts/reviews/', UserReviewsListView.as_view(), name="user_reviews"),
