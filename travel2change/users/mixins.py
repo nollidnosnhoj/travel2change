f74e2f7d118ecd767dbaf88610e81d72062935b6
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-04-13 18:30:39 -1000

    removing unncessary mixins

--- travel2change/users/mixins.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import PermissionDenied
-from users.models import Host
-
-class UserIsHostMixin(object):
-    def dispatch(self, request, *args, **kwargs):
-        host = Host.objects.filter(user=request.user)
-        if not host:
-            raise PermissionDenied
-        return super().dispatch(request, *args, **kwargs)
