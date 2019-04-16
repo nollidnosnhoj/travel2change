557b97c12e08b3f34c77cf9c0def5dbfae78e5a8
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-04-15 16:59:23 -1000

    bug fix: permissions

--- travel2change/moderations/mixins.py
+++ travel2change/moderations/mixins.py
@@ -8,6 +8,6 @@ class StaffUserOnlyMixin(object):
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return HttpResponseRedirect(settings.LOGIN_URL)
-        if not request.user.is_staff or not request.user.is_superuser:
+        if not request.user.is_staff and not request.user.is_superuser:
             raise PermissionDenied
         return super().dispatch(request, *args, **kwargs)
