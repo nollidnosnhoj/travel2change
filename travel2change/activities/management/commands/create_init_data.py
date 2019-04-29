464c5a8ee9944aad8b690b7e3797afe0180a7229
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-04-28 21:44:39 -1000

    create mod group in init scripts

--- travel2change/activities/management/commands/create_init_data.py
+++ travel2change/activities/management/commands/create_init_data.py
@@ -1,11 +1,25 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission, Group
 from django.contrib.sites.models import Site
 from django.utils.text import slugify
 from activities.models import Region, Tag, Category
 
 User = get_user_model()
+
+mod_group_perms = (
+    'view_activity',
+    'add_activity',
+    'change_activity',
+    'delete_activity',
+    'view_host',
+    'change_host',
+    'view_activityphoto',
+    'add_activityphoto',
+    'change_activityphoto',
+    'delete_activityphoto',
+)
 
 regions_name = (
     'Island Of Hawaii',
@@ -52,6 +66,16 @@ class Command(BaseCommand):
         site.domain = settings.SITE_DOMAIN
         site.save()
         self.stdout.write('Site Information Updated.')
+
+        # create moderation group
+        mod_group, created = Group.objects.get_or_create(name="Moderators")
+        if created:
+            for i in mod_group_perms:
+                mod_group.permissions.add(Permission.objects.get(codename=i))
+                self.stdout.write('{0} permission is added.'.format(i))
+            self.stdout.write(self.style.SUCCESS('{0} group created!'.format(mod_group.name)))
+        else:
+            self.stdout.write('{0} already created'.format(mod_group.name))
 
         # Generate regions, categories, and tags
         for i in range(len(regions_name)):
