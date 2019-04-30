440ff6f8c91d6f137c5d8c42b1432658372abfd1
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-04-30 01:24:24 -1000

    static-files

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,11 @@ from __future__ import absolute_import, print_function, unicode_literals
 from cms.sitemaps import CMSSitemap
 from django.conf import settings
 from django.conf.urls import include, url
-from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views import static
 from django.urls import path
 
 admin.autodiscover()
@@ -27,8 +28,16 @@ urlpatterns += i18n_patterns(
     url(r'^', include('cms.urls')),
 )
 
+media_patterns = [
+    url(r'^media/(?P<path>.*)$', static.serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += media_patterns
+
+urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
     import debug_toolbar
