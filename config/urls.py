53948df87f105331180dcb7c9e63c0d9be3c2bdb
Randall D. Johnson <nollidnosnhoj@gmail.com>
2019-05-03 17:16:34 -1000

    refactor

--- config/urls.py
+++ config/urls.py
@@ -8,17 +8,26 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path
+from django.utils.decorators import method_decorator
+from allauth.account.views import LoginView
+from axes.decorators import axes_dispatch
+from axes.decorators import axes_form_invalid
 from activities.sitemap import ActivitySitemap
+from users.forms import LoginForm
 
 admin.autodiscover()
+
+LoginView.dispatch = method_decorator(axes_dispatch)(LoginView.dispatch)
+LoginView.form_invalid = method_decorator(axes_form_invalid)(LoginView.form_invalid)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),  # NOQA
     url(r'^favorites/', include('favorites.urls')),
     url(r'^reviews/', include('reviews.urls')),
     url(r'^moderations/', include('moderations.urls')),
-    url(r'^', include('users.urls')),
+    url(r'^accounts/login/', LoginView.as_view(form_class=LoginForm), name='account_login'),
     url(r'^accounts/', include('allauth.urls')),
+    url(r'^', include('users.urls')),
     url(r'^', include('cms.urls')),
 ]
 
--- travel2change/users/urls.py
+++ travel2change/users/urls.py
@@ -1,9 +1,4 @@
 from django.urls import path
-from allauth.account.views import LoginView
-from axes.decorators import axes_dispatch
-from axes.decorators import axes_form_invalid
-from django.utils.decorators import method_decorator
-from users.forms import LoginForm
 from users.views import (
     HostReviewsListView,
     HostActivitiesListView,
@@ -14,10 +9,6 @@ from users.views import (
     UserReviewsListView,
 )
 
-LoginView.dispatch = method_decorator(axes_dispatch)(LoginView.dispatch)
-LoginView.form_invalid = method_decorator(axes_form_invalid)(LoginView.form_invalid)
-
-
 urlpatterns = [
     path('hosts/<slug:slug>/reviews/', HostReviewsListView.as_view(), name='host_reviews'),
     path('hosts/<slug:slug>/activities/', HostActivitiesListView.as_view(), name="host_activities"),
@@ -25,6 +16,5 @@ urlpatterns = [
     path('accounts/reviews/', UserReviewsListView.as_view(), name="user_reviews"),
     path('accounts/host/activities/', HostAccountActivitiesListView.as_view(), name="host_activities_dashboard"),
     path('accounts/host/', HostUpdateView.as_view(), name="host_update"),
-    path('accounts/login/', LoginView.as_view(form_class=LoginForm), name='account_login'),
     path('accounts/', UserUpdateView.as_view(), name='user_update'),
 ]
